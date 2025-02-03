46a4c685a2a3ffff27502cadc4ead0a11f1fe01e
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-02-03 08:09:55 +0800

    init comit

---
+++ src/contexts/CardContext.ts
@@ -0,0 +1,14 @@
+// 1). import essential hooks for creating context
+// - 
+
+// 2). Define Context Type
+// - 
+
+// 3). Create Context
+// - 
+
+// 4). Provider Component
+// - 
+
+// 5). Custom Hook for consuming context
+// - 
